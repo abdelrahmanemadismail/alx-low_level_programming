930419c945f481d4ef36573c0043601b00084556
abdelrahman.e.ismail <abdelrahman.e.ismail@gmail.com>
2023-02-16 12:37:16 -0800

    Create 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-#include <string.h>
-int main()
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-char str1[52];
-strcpy(str1, "\"Programming is like building a multilingual puzzle");
-puts(str1);
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
