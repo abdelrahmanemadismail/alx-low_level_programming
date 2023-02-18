62fe728d5c15a385ff20981349ea282d5b1a2efe
abdelrahman.e.ismail <abdelrahman.e.ismail@gmail.com>
2023-02-17 18:17:05 -0800

    Fix 2 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,8 +15,13 @@ for (i = 0; i < 99 ; i++)
 		putchar((i / 10) + '0');
 		putchar((i % 10) + '0');
 		putchar(' ');
+		putchar((j / 10) + '0');
 		putchar((j % 10) + '0');
-		putchar((j % 10) + '0');
+		if (i != 98 || j != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 }
 putchar('\n');
