19babaf471b168f13cdf460c71323da77fd2bf2b
abdelrahman.e.ismail <abdelrahman.e.ismail@gmail.com>
2023-02-23 15:24:56 -0800

    Fix 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,25 +6,14 @@
  */
 void print_number(int n)
 {
-unsigned int num;
-int d;
-d = 1;
-num = n;
-if (n < 0)
-{
-	_putchar('-');
-}
-
-while (num / d >= 10)
-{
-	d *= 10;
-}
-while (d >= 1)
-{
-	int c;
-
-	c = (num / d) % 10;
-	_putchar(c + '0');
-	d /= 10;
-}
+	unsigned int m = n;	
+	if (n < 0)
+	{
+		_putchar('-');
+	}
+	if (m / 10 != 0)
+	{
+		print_number(m / 10);
+	}
+	_putchar((m % 10) + '0');
 }
