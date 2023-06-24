e459576cd5823cbd7c847f3454685229cc1e67b3
abdelrahman-e-ismail <abdelrahman.e.ismail@gmail.com>
2023-06-24 06:13:24 -0700

    Create 1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *  _strlen - function that returns the length of a string.
+ * @s: string to calculate it's length
+ * Return: the length of string
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+	char *p = s;
+
+	while (*(p + c) != '\0')
+	{
+		c++;
+	}
+return (c);
+}
+
+/**
+ * string_nconcat - a function that concatenates two strings.
+ * @s1: The First string
+ * @s2: The Second string
+ * @n: the number of charaters to be concatenated from the second string
+ * Return: pointer shall point to a newly allocated space in memory.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p;
+	unsigned int c1, c2;
+	unsigned int i, j;
+
+	c1 = _strlen(s1) - 1;
+	c2 = _strlen(s2) - 1;
+	if (n < c2)
+		c2 = n;
+	p = malloc(c1 + c2 + 1);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= c1; i++)
+	{
+		p[i] = s1[i];
+	}
+	for (j = 0; j < c2; j++)
+	{
+		p[i] = s2[j];
+		i++;
+	}
+	p[i] = '\0';
+	return (p);
+}
