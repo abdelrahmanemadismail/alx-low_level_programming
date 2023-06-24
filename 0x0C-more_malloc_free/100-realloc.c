e41ac6f811789950ea24c3678dc53fbd1ad41fcb
abdelrahman-e-ismail <abdelrahman.e.ismail@gmail.com>
2023-06-24 06:13:25 -0700

    Create 100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _realloc - function that reallocates a memory block using malloc and free
+ * @ptr : is a pointer to the memory previously allocated
+ * @old_size : is the size, in bytes, of the allocated space for ptr
+ * @new_size : is the new size, in bytes of the new memory block
+ * Return: new pointer to the newly created array
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *p;
+	char *n;
+	char *o = ptr;
+	unsigned int smaller = old_size;
+	unsigned int i;
+
+	if (ptr == NULL)
+	{
+		return (malloc(new_size));
+	}
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	else if (old_size > new_size)
+	{
+		smaller = new_size;
+	}
+	p = malloc(new_size);
+	n = p;
+	for (i = 0; i < smaller; i++)
+	{
+		n[i] = o[i];
+	}
+	free(ptr);
+	return (p);
+}
