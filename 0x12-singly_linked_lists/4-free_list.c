af0161a2e21cc2000a9a8f85bebc44e2fdf321f6
abdelrahman-e-ismail <abdelrahman.e.ismail@gmail.com>
2023-06-30 04:23:58 -0700

    Create 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * free_node - function that frees a list_t node.
+ * @node: node to be freed
+ * Return: nothing
+ */
+void free_node(list_t *node)
+{
+	free(node->str);
+	free(node);
+}
+/**
+ * free_list - function that frees a list_t list.
+ * @head: the head of the linked list
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *p = head;
+
+	if (p->next == NULL)
+		free_node(p);
+	else
+	{
+		free_list(p->next);
+		free_node(p);
+	}
+}
